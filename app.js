d8af62b7738906dec63c981d74c6cba729916359
Patrick Ford <patrickford@msn.com>
2016-09-26 00:36:39 -0700

    Added comments and code clean up

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 
-  // Add item to list
+  // Add item to list on form submit
   $('#myForm').submit(function(event) {
+    // Prevent page from doing automatic refresh
     event.preventDefault();
+
+    // Get value from input field
     var input = $('#myInput').val();
+
+    // Clear input field
     $('#myInput').val('');
+
+    // Build HTML for appending item to list
     var html = "";
     html += '<li class="item">';
-    html += '<button class="done">X</button>';
+    html += '<button class="done">X</button>&nbsp;';
     html += input;
     html += '<button class="remove">Remove</button>';
     html += '</li>';
@@ -16,14 +23,12 @@ $(document).ready(function() {
 
   // Remove item from list
   $(document).on('click', '.remove', function(e) {
-    var what = $(this).parent();
-    what.remove();
+    $(this).parent().remove();
   });
 
   // Mark item as complete by adding line-through
   $(document).on('click', '.done', function(e) {
-    var what = $(this).parent();
-    what.addClass('strike');
+    $(this).parent().addClass('strike');
   });
 
 });
